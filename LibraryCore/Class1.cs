69182ba350a317b166bacf507c4af45e761f09fa
Sergey Derko <sergeyderko@gmail.com>
2021-07-08 10:43:56 +0300

    Add new progects

---
+++ LibraryCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LibraryCore
+{
+    public class Class1
+    {
+    }
+}
---
+++ ServiceCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
